d1c0dfb9aeb5e70688864a6f3d6c805f90d3e3df
RinxSR <rinxsr@gmail.com>
2016-03-23 22:42:09 +0300

    создание колоды карт для BlackJack

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -3,14 +3,52 @@ package model;
 import java.util.ArrayList;
 
 public class Game {
-    ArrayList<Card> playingCards;
-    ArrayList<Card> muckCards;
 
+    private ArrayList<Card> playingCards;
+    private ArrayList<Card> muckCards;
+    private final int DECK_AMOUNT = 6;
 
+    public ArrayList<Card> getPlayingCards() {
+        return playingCards;
+    }
+
+    public void setPlayingCards(ArrayList<Card> playingCards) {
+        this.playingCards = playingCards;
+    }
+
+    public ArrayList<Card> getMuckCards() {
+        return muckCards;
+    }
+
+    public void setMuckCards(ArrayList<Card> muckCards) {
+        this.muckCards = muckCards;
+    }
+
+    public Game() {
+
+        playingCards = new ArrayList<Card>();
+        createPlayingCard();
+
+        muckCards = new ArrayList<Card>();
+
+    }
+
+    public void createDeck() {
+        for (Suit suit : Suit.values()) {
+            for (Value value : Value.values()) {
+                playingCards.add(new Card(suit, value));
+            }
+        }
+    }
+
+    public void createPlayingCard() {
+        for (int i = 0; i < DECK_AMOUNT; i++) createDeck();
+    }
 
     public void startGame() {
 
         playingCards = new ArrayList<Card>();
 
     }
+
 }
