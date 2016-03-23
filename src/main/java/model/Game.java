94aad78dd723ffb8b77e4e8e3c03b9547795c5d8
RinxSR <rinxsr@gmail.com>
2016-03-23 22:09:12 +0300

    создал проект и начал набрасывать классы

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<Card> playingCards;
+    ArrayList<Card> muckCards;
+
+
+
+    public void startGame() {
+
+        playingCards = new ArrayList<Card>();
+
+    }
+}
