0745affe3700182499b6586baade56961a17a318
RinxSR <rinxsr@gmail.com>
2016-03-24 14:08:17 +0300

    Первоначальная раздача карт + подсчет очков + показ игрового стола

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -4,10 +4,52 @@ public class Card {
 
     private Suit suit;
     private Value value;
+    private int rating;
 
     public Card(Suit suit, Value value) {
         this.suit = suit;
         this.value = value;
+        switch (value) {
+            case TWO:
+                rating = 2;
+                break;
+            case THREE:
+                rating = 3;
+                break;
+            case FOUR:
+                rating = 4;
+                break;
+            case FIVE:
+                rating = 5;
+                break;
+            case SIX:
+                rating = 6;
+                break;
+            case SEVEN:
+                rating = 7;
+                break;
+            case EIGHT:
+                rating = 8;
+                break;
+            case NINE:
+                rating = 9;
+                break;
+            case TEN:
+                rating = 10;
+                break;
+            case JACK:
+                rating = 10;
+                break;
+            case QUEEN:
+                rating = 10;
+                break;
+            case KING:
+                rating = 10;
+                break;
+            case ACE:
+                rating = 11;
+                break;
+        }
     }
 
     public Suit getSuit() {
@@ -25,5 +67,13 @@ public class Card {
 
     public void setValue(Value value) {
         this.value = value;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 
 public class Game {
 
-    private ArrayList<Card> playingCards;
+    private ArrayList<Card> playingDeck;
     private ArrayList<Card> muckCards;
+    private ArrayList<Card> playerCards;
+    private ArrayList<Card> dealerCards;
     private final int DECK_AMOUNT = 6;
 
-    public ArrayList<Card> getPlayingCards() {
-        return playingCards;
+    private int dealerTotal = 0;
+    private int playerTotal = 0;
+
+    public ArrayList<Card> getPlayingDeck() {
+        return playingDeck;
     }
 
-    public void setPlayingCards(ArrayList<Card> playingCards) {
-        this.playingCards = playingCards;
+    public void setPlayingDeck(ArrayList<Card> playingDeck) {
+        this.playingDeck = playingDeck;
     }
 
     public ArrayList<Card> getMuckCards() {
@@ -26,29 +31,77 @@ public class Game {
 
     public Game() {
 
-        playingCards = new ArrayList<Card>();
-        createPlayingCard();
+        playingDeck = new ArrayList<Card>();
+        createPlayingDeck();
 
         muckCards = new ArrayList<Card>();
+        playerCards = new ArrayList<Card>();
+        dealerCards = new ArrayList<Card>();
 
+        giveCardToDealer(getRandomCard());
+
+        giveCardToPlayer(getRandomCard());
+        giveCardToPlayer(getRandomCard());
     }
 
     public void createDeck() {
         for (Suit suit : Suit.values()) {
             for (Value value : Value.values()) {
-                playingCards.add(new Card(suit, value));
+                playingDeck.add(new Card(suit, value));
             }
         }
     }
 
-    public void createPlayingCard() {
+    public void createPlayingDeck() {
         for (int i = 0; i < DECK_AMOUNT; i++) createDeck();
     }
 
     public void startGame() {
 
-        playingCards = new ArrayList<Card>();
-
     }
+
+    public Card getRandomCard() {
+        int random = (int) (Math.random() * playingDeck.size());
+        Card card = playingDeck.get(random);
+        playingDeck.remove(random);
+        return card;
+    }
+
+    public void giveCardToPlayer(Card card) {
+        playerCards.add(card);
+        if (card.getValue() == Value.ACE && (playerTotal + card.getRating() > 21)) {
+            card.setRating(1);
+        }
+        playerTotal += card.getRating();
+    }
+
+    public void giveCardToDealer(Card card) {
+        dealerCards.add(card);
+        if (card.getValue() == Value.ACE && (dealerTotal + card.getRating() > 21)) {
+            card.setRating(1);
+        }
+        dealerTotal += card.getRating();
+    }
+
+    public void showTable() {
+
+
+        System.out.println("Dealer's cards: ");
+        for (Card dealerCard : dealerCards) {
+            System.out.print(" <" + dealerCard.getValue() + " " + dealerCard.getSuit() + "> ");
+        }
+        System.out.println();
+        System.out.println("Total: " + dealerTotal);
+        System.out.println();
+
+        System.out.println("Player's cards: ");
+        for (Card playerCard : playerCards) {
+            System.out.print(" <" + playerCard.getValue() + " " + playerCard.getSuit() + "> ");
+        }
+        System.out.println();
+        System.out.println("Total: " + playerTotal);
+        System.out.println();
+    }
+
 
 }
